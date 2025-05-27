2585456c508bba766e167f7c340646d3184c1d8a
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-05-27 11:13:53 +0100

    feat: fundaments js

---
+++ JavaScript/fundaments/basic.js
@@ -0,0 +1,26 @@
+var welcome = "Hello, World Robson de Jesus!";
+// console.log(welcome);
+
+var curso = "JavaScript Fundamentos";
+console.log(curso);
+
+curso = "Curso de JavaScript";
+console.log(curso + " - " + welcome);
+// alert(curso);
+
+var valor1 = 7;
+var valor2 = 5;
+var soma = valor1 + valor2;
+var media = (valor1 + valor2) / 2;
+console.log(valor1);
+console.log(soma);
+console.log("Media: "+media);
+
+var nome = "Matheus"
+var NOME = "Robson de Jesus";
+console.log("O nome é: " + NOME);
+
+var val1 = 10; 
+var val2 = 5;
+var resultado = val1 + val2;
+console.log("Resultado: " + resultado)
---
+++ JavaScript/fundaments/script.js
@@ -0,0 +1,11 @@
+var nome = prompt("Qual o seu nome?");
+
+var sobrenome = prompt("Qual o seu sobrenome?");
+console.log("Olá " + nome + " " + sobrenome);
+
+
+// document.write("<h1>Bem-Vindo " + nome + " ao projeto</h1>"); 
+document.writeln("<h1>Bem-Vindo " + nome + " ao projeto</h1>");
+
+
+document.write("<img src='https://picsum.photos/200/300' alt='Imagem aleatória'>");
