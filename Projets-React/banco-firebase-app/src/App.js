068b5a613be458bad70c81efacd47fd49815250d
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-07-17 09:11:45 +0100

    feat: update date and delete

--- Projets-React/banco-firebase-app/src/App.js
+++ Projets-React/banco-firebase-app/src/App.js
@@ -1,17 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "./firebaseConnection";
-import {addDoc, collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import "./style.css";
 
 function App() {
   const [titulo, setTitulo] = useState("");
   const [autor, setAutor] = useState("");
+  const [idPost, setIdPost] = useState("");
 
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function loadPosts() {
+      const unsub = onSnapshot(collection(db, "posts"), (snapshot) => {
+        let listaPost = [];
+
+        snapshot.forEach((doc) => {
+          listaPost.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          });
+        });
+        setPosts(listaPost);
+      });
+    }
+    loadPosts();
+  });
+
+
+  //  useEffect(() => {
+  //   buscarPost();
+  // }, [autor, titulo])
+
   async function handleAdd() {
     // await setDoc(doc(db, "posts", "12345"), {
-    //   titulo: titulo, 
+    //   titulo: titulo,
     //   autor: autor,
     // })
 
@@ -22,57 +56,95 @@ function App() {
     //   console.log("Error de cadastro:", error)
     // })}
     await addDoc(collection(db, "posts"), {
-      titulo: titulo, 
+      titulo: titulo,
       autor: autor,
     })
-    .then(() => {
-      console.log("Cadastrado com sucesso");
-      setTitulo("");
-      setAutor("");
-    })
-    .catch((error) => {
-      console.log("Error de cadastro:", error);
-    })
-}
-
-
-async function buscarPost() {
-  // const postRef = doc(db, "posts", "cx9zYbTrPP0k6KA7AeaS")
-  // await getDoc(postRef)
-  // .then((snapshot) => {
-  //   setAutor(snapshot.data().autor);
-  //   setTitulo(snapshot.data().titulo);
-  // })
-  // .catch((error) => {
-  //   console.log("Error ao buscar post", error)
-  // })
-
-  const postRef = collection(db, "posts")
-  await getDocs(postRef)
-
-  .then((snapshot) => {
-    let lista = [];
-
-    snapshot.forEach((doc) => {
-      lista.push({
-        id: doc.id,
-        titulo: doc.data().titulo, 
-        autor: doc.data().autor
+      .then(() => {
+        console.log("Cadastrado com sucesso");
+        setTitulo("");
+        setAutor("");
       })
-    })
-    setPosts(lista);
-  })
-  .catch((error) => {
-    console.log("Error ao buscar posts", error);
-  })
+      .catch((error) => {
+        console.log("Error de cadastro:", error);
+      });
+  }
 
-}
+  async function buscarPost() {
+    // const postRef = doc(db, "posts", "cx9zYbTrPP0k6KA7AeaS")
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor);
+    //   setTitulo(snapshot.data().titulo);
+    // })
+    // .catch((error) => {
+    //   console.log("Error ao buscar post", error)
+    // })
+
+    const postRef = collection(db, "posts");
+    await getDocs(postRef)
+      .then((snapshot) => {
+        let lista = [];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          });
+        });
+        setPosts(lista);
+      })
+      .catch((error) => {
+        console.log("Error ao buscar posts", error);
+      });
+  }
+
+  async function editarPost() {
+    const docRef = doc(db, "posts", idPost);
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor,
+    })
+      .then(() => {
+        alert("Post atualizado com sucesso");
+        setIdPost("");
+        setTitulo("");
+        setAutor("");
+        buscarPost();
+      })
+      .catch((error) => {
+        console.log("Error ao atualizar post", error);
+      });
+  }
+
+  async function excluirPost(id) {
+    const docRef = doc(db, "posts", id);
+    await deleteDoc(docRef, {
+      titulo: titulo,
+      autor: autor,
+    })
+      .then(() => {
+        alert("Post excluído com sucesso");
+        buscarPost();
+      })
+      .catch((error) => {
+        console.log("Error ao excluir post", error);
+      });
+  }
 
   return (
     <div className="App">
       <h1>React js + FireBase</h1>
 
       <div className="container">
+        <label>Id do Post:</label>
+        <input
+          type="text"
+          placeholder="Digite o id do post"
+          value={idPost}
+          onChange={(e) => setIdPost(e.target.value)}
+        />{" "}
+        <br />
         <label>Titulo:</label>
         <textarea
           type="text"
@@ -81,19 +153,29 @@ async function buscarPost() {
           onChange={(e) => setTitulo(e.target.value)}
         ></textarea>
         <label>Autor:</label>
-        <input type="text" placeholder="Autor do post" value={autor} onChange={(e) => setAutor(e.target.value)}/>
-
+        <input
+          type="text"
+          placeholder="Autor do post"
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
+        />
         <button onClick={handleAdd}>Cadastrar</button>
         <button onClick={buscarPost}>Buscar post</button>
-
+        <br />
+        <button onClick={editarPost}>Atualizar Post</button>
         <ul>
           {posts.map((post) => {
             return (
               <li key={post.id}>
+                <strong>ID: {post.id}</strong> <br />
                 <span>Titulo: {post.titulo}</span> <br />
-                <span>Autor: {post.autor}</span> <br /> <br />
+                <span>Autor: {post.autor}</span> <br />
+                <button onClick={() => excluirPost(post.id)}>
+                  Excluir
+                </button>{" "}
+                <br /> <br />
               </li>
-            )
+            );
           })}
         </ul>
       </div>
