7e47fb38436c8619fc9f31c49bfcd5a80a005060
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-07-18 10:34:22 +0100

    feat: auth system

--- Projets-React/banco-firebase-app/src/App.js
+++ Projets-React/banco-firebase-app/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { db } from "./firebaseConnection";
+import { db, auth } from "./firebaseConnection";
 import {
   addDoc,
   collection,
@@ -11,12 +11,18 @@ import {
   setDoc,
   updateDoc,
 } from "firebase/firestore";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import "./style.css";
 
 function App() {
   const [titulo, setTitulo] = useState("");
   const [autor, setAutor] = useState("");
   const [idPost, setIdPost] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const [user, setUser] = useState(false);
+  const [userDetails, setUserDetails] = useState({});
 
   const [posts, setPosts] = useState([]);
 
@@ -132,11 +138,92 @@ function App() {
       });
   }
 
+  async function novoUsuario() {
+    await createUserWithEmailAndPassword(auth, email, senha)
+    .then((value) => {
+      alert("Cadastrado com sucesso");
+      setEmail("");
+      setSenha("");
+      console.log(value);
+    })
+    .catch((error) => {
+      if(error.code === "auth/weak-password") {
+        alert("Senha muito fraca");
+      }else if (error.code === "auth/email-already-in-use"){
+        alert("Email já cadastrado");
+      }
+    })  
+  }
+
+  async function logarUsuario() {
+    await signInWithEmailAndPassword(auth, email, senha)
+    .then((value) => {
+      alert("Login realizado com sucesso");
+      console.log(value.user)
+
+      setUserDetails({
+        uid: value.user.uid, 
+        email: value.user.email,
+      })
+
+      setUser(true);
+
+      setEmail("");
+      setSenha("");
+      
+    })
+    .catch((error) => {
+      console.log("Error ao fazer login: ", error);
+    })
+  }
+
+  async function fazerLogout() {
+    await signOut(auth)
+    .then(() => {
+      setUser(false);
+      setUserDetails({});
+      alert("Logout realizado com sucesso");
+    })
+
+    .catch((error) => {
+      console.log("Error ao fazer logout: ", error);
+    })  
+  }
+
   return (
     <div className="App">
       <h1>React js + FireBase</h1>
 
+     {user && (
+      <div>
+        <strong>Seja bem vindo(a) voce está logado</strong> <br />
+        <span>ID: {userDetails.uid}</span> <br />
+        <span>Email: {userDetails.email}</span> <br /> 
+        
+        <button onClickCapture={fazerLogout}>Sair da conta</button>
+      </div>
+     )}
+
+
       <div className="container">
+        <h2>Usuários</h2>
+        <label>Email</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Digite o seu email" /> <br />
+
+
+        <label>Senha: </label>
+        <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} placeholder="Digite a sua senha" />
+        <br />
+
+        <button onClick={novoUsuario}>Cadastrar</button>
+        <button onClick={logarUsuario}>Fazer Login</button>
+        
+      </div>
+      <br /> <br />
+      <hr />
+
+      <div className="container">
+        <h2>Posts</h2>
         <label>Id do Post:</label>
         <input
           type="text"
--- Projets-React/banco-firebase-app/src/firebaseConnection.js
+++ Projets-React/banco-firebase-app/src/firebaseConnection.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -16,6 +17,7 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 
-export { db};
+export { db, auth};
