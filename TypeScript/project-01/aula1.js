f1629a855f52d0f16d3a0eff965cc89ca3d281a8
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-07-21 09:13:41 +0100

    feat: class typecript

---
+++ TypeScript/project-01/aula1.js
@@ -0,0 +1,13 @@
+"use strict";
+//esse é um comentário
+const nome = "Robson";
+console.log("Bem vindo " + nome);
+class Sistema {
+    constructor(ip) {
+        this.ip = ip;
+    }
+}
+// teste
+// teste
+// teste 
+const servidor1 = new Sistema("192.168.0.1");
---
+++ TypeScript/project-01/aula1.ts
@@ -0,0 +1,19 @@
+//esse é um comentário
+const nome = "Robson";
+console.log("Bem vindo " + nome);
+
+
+class Sistema {
+  ip: string;
+
+
+  constructor(ip: string) {
+    this.ip = ip;
+  }
+}
+
+// teste
+// teste
+// teste 
+
+const servidor1 = new Sistema("192.168.0.1");
