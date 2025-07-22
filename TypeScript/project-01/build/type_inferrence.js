2a980b0d95d59488727f2340c82c47791f4a4b7f
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-07-22 08:22:07 +0100

    feat: type inferrence

---
+++ TypeScript/project-01/build/type_inferrence.js
@@ -0,0 +1,14 @@
+"use strict";
+//tipagem dinamica 
+let curso = "Curso de TypeScript";
+let tecnologias = [
+    "PHP",
+    "React JS",
+    20,
+    false
+];
+tecnologias.push("Node JS");
+tecnologias.push(10);
+tecnologias.push(true);
+console.log(tecnologias);
+console.log(curso);
